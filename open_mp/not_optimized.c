4e88e67109138eb2b3d202c43f286a950e4be52e
ola567 <aleksandra.hein@o2.pl>
2024-10-24 22:21:56 +0200

    add generating matrixes with given dimensions

--- open_mp/not_optimized.c
+++ open_mp/not_optimized.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <time.h>
 #include "utils.h"
 #include "omp.h"
 
+
+void generate_matrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = rand() % 100;
+        }
+    }
+}
+
+int** allocate_matrix(int rowsA, int colsB) {
+    int **C = (int**)malloc(rowsA * sizeof(int*));
+    if (C == NULL) {
+        printf("Result matrix memory allocation error\n");
+        exit(1);
+    }
+    
+    for (int i = 0; i < rowsA; i++) {
+        C[i] = (int*)calloc(colsB, sizeof(int));
+        if (C[i] == NULL) {
+            printf("Result matrix memory allocation error\n");
+            exit(1);
+        }
+    }
+    return C;
+}
+
+void free_matrix(int** matrix, int rows) {
+    for (int i = 0; i < rows; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+void print_matrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 void read_matrices_from_file(const char* filename, int*** A, int*** B, int* rowsA, int* colsA, int* rowsB, int* colsB) {
     FILE *file = fopen(filename, "r");
@@ -39,43 +81,31 @@ void read_matrices_from_file(const char* filename, int*** A, int*** B, int* rows
     fclose(file);
 }
 
-int** allocate_result_matrix(int rowsA, int colsB) {
-    int **C = (int**)malloc(rowsA * sizeof(int*));
-    if (C == NULL) {
-        printf("Result matrix memory allocation error\n");
-        exit(1);
-    }
-    
-    for (int i = 0; i < rowsA; i++) {
-        C[i] = (int*)calloc(colsB, sizeof(int));
-        if (C[i] == NULL) {
-            printf("Result matrix memory allocation error\n");
-            exit(1);
-        }
-    }
-    return C;
-}
-
-void print_matrix(int** matrix, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 int main(int argc, char **argv) {
     int **A, **B, **C;
     int rowsA, colsA, rowsB, colsB;
     long long threads_nr = 10;
+    srand(time(NULL));
 
     // get input data
-    read_matrices_from_file("../matrixes.txt", &A, &B, &rowsA, &colsA, &rowsB, &colsB);
+    // read_matrices_from_file("../matrixes.txt", &A, &B, &rowsA, &colsA, &rowsB, &colsB);
+    // print_matrix(A, rowsA, colsA);
+    // print_matrix(B, rowsB, colsB);
+
+    // generate matrixes
+    rowsA = atoi(argv[1]);
+    colsA = atoi(argv[2]);
+    rowsB = atoi(argv[3]);
+    colsB = atoi(argv[4]);
+    A = allocate_matrix(rowsA, colsA);
+    B = allocate_matrix(rowsB, colsB);
+    generate_matrix(A, rowsA, colsA);
+    generate_matrix(B, rowsB, colsB);
     print_matrix(A, rowsA, colsA);
     print_matrix(B, rowsB, colsB);
+
     // allocate memory for result data
-    C = allocate_result_matrix(rowsA, colsB);
+    C = allocate_matrix(rowsA, colsB);
 
     // set number of threads
     omp_set_num_threads(rowsA * colsB);
@@ -104,12 +134,9 @@ int main(int argc, char **argv) {
     ins__printtime(&ins__tstart, &ins__tstop);
 
     // free memory
-    for (int i = 0; i < rowsA; i++) {free(A[i]);}
-    free(A);
-    for (int i = 0; i < rowsB; i++) {free(B[i]);}
-    free(B);
-    for (int i = 0; i < rowsA; i++) {free(C[i]);}
-    free(C);
+    free_matrix(A, rowsA);
+    free_matrix(B, rowsB);
+    free_matrix(C, rowsA);
 
     return 0;
 }
