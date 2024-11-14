95409f2d37dbada8ff4ebc6a19b9676221248521
ola567 <aleksandra.hein@o2.pl>
2024-11-14 20:42:56 +0100

    fix experiment filenames

--- main.c
+++ main.c
@@ -9,19 +9,19 @@ int main(int argc, char **argv) {
     int **A, **B, **C;
     struct timeval ins__tstart, ins__tstop;
     int dimensions[10] = {200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000};
-    char filename[4];
+    char experiment_file_path[100];
     int rowsA, colsA, rowsB, colsB;
     FILE *result_file = fopen("results.txt", "w");
     if (result_file == NULL) {
         printf("Cannot open the file\n");
         exit(1);
     }
-    
-    for (int i = 0; i < 10; i++) {
+
+    for (int i = 1; i < 10; i++) {
         fprintf(result_file, "Matrix dimension: %d\n", dimensions[i]);
-        for (int j = 0; j < 10; j++) {
-            sprintf(filename, "%d", dimensions[i]);
-            read_matrices_from_file("experiment_data/200.txt", &A, &B, &rowsA, &colsA, &rowsB, &colsB);
+        for (int j = 0; j < 2; j++) {
+            sprintf(experiment_file_path, "experiment_data/%d.txt", dimensions[i]);
+            read_matrices_from_file(experiment_file_path, &A, &B, &rowsA, &colsA, &rowsB, &colsB);
             C = allocate_matrix(rowsA, colsB);
             
             // without parallelization
